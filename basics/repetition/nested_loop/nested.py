d786ca9df46135f4b7b0b0f31a513845587a7302
Samual Norman <samualtnorman@gmail.com>
2020-10-20 10:44:36 +0100

    Added code to demonstrate the use of one loop nested is another.

---
+++ basics/repetition/nested_loop/nested.py
@@ -0,0 +1,9 @@
+height = int(input("How many rows should I have? "))
+width = int(input("\nHow many columns should I have? "))
+
+print("\nHere I go:\n")
+
+for _ in range(height):
+	print(":-)" * width)
+
+print("\nDone!")
