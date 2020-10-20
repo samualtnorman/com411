4b04a20b0176b7ca12803748966dee1f7b61bbf6
Samual Norman <samualtnorman@gmail.com>
2020-10-20 09:23:46 +0100

    Added code to demonstrate the use of summing with a while loop.

---
+++ basics/repetitions/while_loop/sum_100.py
@@ -0,0 +1,14 @@
+print("Calculating the sum of the first 100 numbers...")
+
+# Here is a good solution
+# print("...Done! The answer is" , sum(range(1, 101)))
+
+# Here is a bad solution
+sum100 = 0
+i = 1
+
+while i < 11:
+	sum100 += i
+	i += 1
+
+print("...Done! The answer is", sum100)
